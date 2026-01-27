3d6ead0c6ce9d251c0fb5c2ee582e2dade0dfd09
Hisham <linkedin.hisham@gmail.com>
2026-01-27 18:31:59 +0530

    choosing time of the day designed the card counter , show counter

--- src/pages/employee_dashboard/EmployeeDashboard.jsx
+++ src/pages/employee_dashboard/EmployeeDashboard.jsx
@@ -1,10 +1,17 @@
-import React from "react";
+import { MEAL_OPTIONS } from "../../constants/meals";
 import { useAuth } from "../../context/AuthContext";
 import FoodCard from "../../components/FoodCard";
 import "./EmployeeDashboard.css";
+import { useState } from "react";
 function EmployeeDashboard() {
   const { user } = useAuth();
+  // set the meal time
+  const [time, setTime] = useState(Object.keys(MEAL_OPTIONS)[0]);
 
+  // const currentMealKey = foodTimes.lunch;
+
+  const mealTitle = time.charAt(0).toUpperCase() + time.slice(1);
+  const currentMealOption = MEAL_OPTIONS[time];
   // if (!user) {
   //   return <div>User not found</div>;
   // }
@@ -15,8 +22,43 @@ function EmployeeDashboard() {
   //when user exists
   return (
     <div className="employee-dash">
-      <h1>Hello, {user?.username}!</h1>
-      <FoodCard />
+      <h1>Hello, {user?.username ?? "Sarah"}!</h1>
+      <div className="meals-container">
+        <select
+          name="select-time"
+          id="select-time"
+          onChange={(e) => setTime(e.target.value)}
+        >
+          {Object.keys(MEAL_OPTIONS).map((item) => {
+            return (
+              <option key={item} value={item}>
+                {item.charAt(0).toUpperCase() + item.slice(1)}
+              </option>
+            );
+          })}
+        </select>
+        <div className="meals-cnt-title">
+          <h3>Food items for {mealTitle}</h3>
+          <div className="vote-ends-in">
+            <span>Vote ends in</span>
+            <div>01:30pm</div>
+          </div>
+        </div>
+        <div className="food-card-grid">
+          {currentMealOption.map((meal) => (
+            <FoodCard meal={meal} key={meal.id} />
+          ))}
+        </div>
+        <div className="center-btn">
+          <button className="btn-base  btn-negative ">
+            Opt out for {mealTitle}
+          </button>
+        </div>
+      </div>
+
+      <div className="submit-vote-cnt center-btn">
+        <button className="btn-base btn-positive">SUBMIT VOTE</button>
+      </div>
     </div>
   );
 }
