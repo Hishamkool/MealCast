28a47a55369166512461c05fc6f9f13d0667051a
Hisham <linkedin.hisham@gmail.com>
2026-01-29 12:08:46 +0530

    #added entering and exit animations #added timer id to clear previous timers for snackbars #added snackid to attach key to snackbar to for every new snackbar call #added new svgs for variants of snackbar and added css for that.

--- src/components/SnackBar.jsx
+++ src/components/SnackBar.jsx
@@ -1,26 +1,34 @@
-import React, { useContext } from "react";
-import CheckIcon from "../assets/svg/simple_check.svg?react";
+import React, { useContext, useEffect, useState } from "react";
+
+/* svg icons */
+import SuccessIcon from "../assets/svg/check_circle.svg?react";
+import DefaultIcon from "../assets/svg/circle_notifications.svg?react";
+import ErrorIcon from "../assets/svg/error_circle.svg?react";
+import ReportIcon from "../assets/svg/report.svg?react";
+import SubmitIcon from "../assets/svg/send.svg?react";
+import TickIcon from "../assets/svg/simple_check.svg?react";
+import WarningIcon from "../assets/svg/warning.svg?react";
+
 import { SnackBarContext } from "../context/SnackBarContext";
 import "./SnackBar.css";
 
 function SnackBar() {
   const icons = {
-    success: <CheckIcon className="snackbar-icon" />,
-    error: <CheckIcon className="snackbar-icon" />,
-    warning: "⚠️",
-    default: "ℹ️",
+    success: <SuccessIcon className="snackbar-icon" />,
+    error: <ErrorIcon className="snackbar-icon" />,
+    warning: <WarningIcon className="snackbar-icon" />,
+    default: <DefaultIcon className="snackbar-icon" />,
   };
-  const { open, msg, variant } = useContext(SnackBarContext);
+  const { open, exiting, snackId, msg, variant } = useContext(SnackBarContext);
 
-  /* disabling the snackbar which is not opened else present in dom and  prevents clicks   */
-  if (!open) {
-    return null;
-  }
+  if (!open) return null;
+
   return (
-    <div className={`snackbar ${variant}`}>
-      <span>
-        {icons[variant]} {msg}
-      </span>
+    <div
+      key={snackId}
+      className={`snackbar ${variant} ${exiting ? "exit" : "enter"}`}
+    >
+      {icons[variant]} {msg}
     </div>
   );
 }
